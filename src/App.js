e1f7c68f783edad0f818aec939ebbccc47f6c532
elenvlas <argex@ukr.net>
2024-01-07 17:17:13 +0200

    Added link of hosted on Netlify

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <div className="container">
        <Weather defaultCity="Kharkiv" />       
         <footer>
-          This project was coded by{" "}<a href='http://www.vlasenko.io' target='_blank' rel="noreferrer">Elena Vlasenko</a>{" "}and is{" "}<a href='https://github.com/elenvlas/react-weather-app' target='_blank' rel="noreferrer">open-sourced on GitHub</a> 
+          This project was coded by{" "}<a href='http://www.vlasenko.io' target='_blank' rel="noreferrer">Elena Vlasenko</a>{" "}and is{" "}<a href='https://github.com/elenvlas/react-weather-app' target='_blank' rel="noreferrer">open-sourced on GitHub</a>{" "}and{" "}<a href='https://master--jade-hamster-0c9da4.netlify.app/' target='_blank' rel="noreferrer">hosted on Netlify</a> 
         </footer>
       </div>
     </div>
